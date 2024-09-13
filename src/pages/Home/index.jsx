2d4b34f46de3232dc25013946ddd3f08c6ef50bc
guilhermercoutinho <guilhermecoutinho.rc@gmail.com>
2024-09-13 13:20:44 -0300

    Alteração nas estruturas e pastas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,9 +8,18 @@ import {
   Title,
   TopBackground,
 } from "./style";
-import Users from "./assets/users.png";
+import Users from "../../assets/users.png";
+import { useRef } from "react";
 
 function Home() {
+  const inputName = useRef();
+  const inputAge = useRef();
+  const inputEmail = useRef();
+
+  function registerNewUser() {
+    console.log(inputName.current.value);
+  }
+
   return (
     <>
       <Container>
@@ -26,24 +35,38 @@ function Home() {
               <InputLabel>
                 Nome<span> *</span>
               </InputLabel>
-              <Input type="text" placeholder="Nome do usuário" />
+              <Input
+                type="text"
+                placeholder="Nome do usuário"
+                ref={inputName}
+              />
             </div>
 
             <div>
               <InputLabel>
                 Idade<span> *</span>
               </InputLabel>
-              <Input type="number" placeholder="Idade do usuário" />
+              <Input
+                type="number"
+                placeholder="Idade do usuário"
+                ref={inputAge}
+              />
             </div>
           </ContainerInputs>
           <div style={{ width: "100%" }}>
             <InputLabel>
               E-mail<span> *</span>
             </InputLabel>
-            <Input type="email" placeholder="E-mail do usuário" />
+            <Input
+              type="email"
+              placeholder="E-mail do usuário"
+              ref={inputEmail}
+            />
           </div>
 
-          <Button>Cadastrar Usuário</Button>
+          <Button type="button" onClick={registerNewUser}>
+            Cadastrar Usuário
+          </Button>
         </Form>
       </Container>
     </>
